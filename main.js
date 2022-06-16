a56c23b349b559e5dc9c4a3adc86126efbca15ba
hits <ductai0201@gmail.com>
2022-06-17 00:14:58 +0700

    update

---
+++ main.js
@@ -0,0 +1,38 @@
+const listMenu =[
+  {id:1, title: 'Home', href: 'index.html'},
+  {id:2, title: 'Products', href: '#'},
+  {id:3, title: 'About Us', href: '#'},
+  {id:4, title: 'Contact Us', href: '#'}
+  
+  ];
+  
+  const listProduct = [
+      {id: 1, title: 'Big and Juicy Wagyu Beef Cheeseburger', src: "/imgs/Mask Group.png"},
+      {id: 2, title: 'Big and Juicy Wagyu Beef Cheeseburger', src: "/imgs/Mask Group (1).png"},
+      {id: 3, title: 'Big and Juicy Wagyu Beef Cheeseburger', src: "/imgs/Mask Group (2).png"},
+      {id: 4, title: 'Big and Juicy Wagyu Beef Cheeseburger', src: "/imgs/Mask Group (3).png"},
+      {id: 5, title: 'Big and Juicy Wagyu Beef Cheeseburger', src: "/imgs/Mask Group (4).png"},
+      {id: 6, title: 'Big and Juicy Wagyu Beef Cheeseburger', src: "/imgs/Mask Group (5).png"},
+  ];
+  
+  
+  
+  const categories = [
+      {id:1, title:'Breakfast', src:"/imgs/Icon/Group 890.svg"},
+      {id:2, title:'Vegan', src:"/imgs/Icon/Group 879.svg"},
+      {id:3, title:'Meat', src:"/imgs/Icon/Group 891.svg"},
+      {id:4, title:'Dessert', src:"/imgs/Icon/Group 892.svg"},
+      {id:5, title:'Lunch', src:"/imgs/Icon/Group 893.svg"},
+      {id:6, title:'Chocolate', src:"/imgs/Icon/Group 894.svg"},
+  ];
+  
+  
+  function showlistMenu(listMenu){
+      if(!Array.isArray(listMenu) || listMenu.length ==0) return false;
+  
+      let result = "";
+      for(let i=0;i<listMenu.length;i++){
+          result +=
+      }
+  
+  }
